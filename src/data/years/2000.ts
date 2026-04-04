a263b76282c6b759b00ba0953200d152272124fe
Saxon Cameron <github.humble368@passinbox.com>
2026-04-04 11:53:40 +1030

    feat: add 2000s chart data

---
+++ src/data/years/2000.ts
@@ -0,0 +1,161 @@
+import { getSongThumbnailPath } from '@/data/imageAsset'
+import type { Song } from '@/types/song'
+
+const year = 2000
+
+export const source = {
+  label: 'ARIA Charts',
+  url: 'https://en.wikipedia.org/wiki/List_of_top_25_singles_for_2000_in_Australia',
+}
+
+const getSong = ({
+  rank,
+  title,
+  artist,
+  album,
+  youtubeVideoId,
+  imageSelection,
+  imageSources,
+}: Omit<Song, 'thumbnailPath'>): Song => ({
+  rank,
+  title,
+  artist,
+  album,
+  youtubeVideoId,
+  imageSelection,
+  imageSources,
+  thumbnailPath: getSongThumbnailPath({ year, rank, title }),
+})
+
+// Source: ARIA year-end singles chart for 2000
+const songs: Song[] = [
+  getSong({
+    rank: 1,
+    title: "I'm Outta Love",
+    artist: 'Anastacia',
+    album: 'Not That Kind',
+    youtubeVideoId: 'TnOy6HEf7HU',
+    imageSelection: 'album',
+    imageSources: {
+      album: 'https://upload.wikimedia.org/wikipedia/en/7/77/Imouttalove.jpg',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 2,
+    title: 'Teenage Dirtbag',
+    artist: 'Wheatus',
+    album: 'Wheatus',
+    youtubeVideoId: 'FC3y9llDXuM',
+    imageSelection: 'album',
+    imageSources: {
+      album:
+        'https://upload.wikimedia.org/wikipedia/en/2/2e/Teenage_Dirtbag.jpg',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 3,
+    title: 'Freestyler',
+    artist: "Bomfunk MC's",
+    album: 'In Stereo',
+    youtubeVideoId: 'ymNFyxvIdaM',
+    imageSelection: 'album',
+    imageSources: {
+      album:
+        'https://upload.wikimedia.org/wikipedia/en/0/09/Bomfunk_-_Freestyler.jpg',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 4,
+    title: 'Music',
+    artist: 'Madonna',
+    album: 'Music',
+    youtubeVideoId: 'Sdz2oW0NMFk',
+    imageSelection: 'album',
+    imageSources: {
+      album:
+        'https://upload.wikimedia.org/wikipedia/en/5/5b/Madonna_music_single.png',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 5,
+    title: 'Say My Name',
+    artist: "Destiny's Child",
+    album: "The Writing's on the Wall",
+    youtubeVideoId: 'sQgd6MccwZc',
+    imageSelection: 'album',
+    imageSources: {
+      album:
+        'https://upload.wikimedia.org/wikipedia/en/7/73/Say_My_Name_%281999_single%29.jpg',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 6,
+    title: 'Poison',
+    artist: 'Bardot',
+    album: 'Bardot',
+    youtubeVideoId: 'aviFNB0Ebx0',
+    imageSelection: 'album',
+    imageSources: {
+      album: 'https://upload.wikimedia.org/wikipedia/en/a/af/Poisonbardot.jpg',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 7,
+    title: 'Bye Bye Bye',
+    artist: '*NSYNC',
+    album: 'No Strings Attached',
+    youtubeVideoId: 'Eo-KmOd3i7s',
+    imageSelection: 'album',
+    imageSources: {
+      album: 'https://upload.wikimedia.org/wikipedia/en/1/14/Bye_Bye_Bye.png',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 8,
+    title: 'Who the Hell Are You',
+    artist: 'Madison Avenue',
+    album: 'The Polyester Embassy',
+    youtubeVideoId: 'r1FFfBp9zsc',
+    imageSelection: 'album',
+    imageSources: {
+      album:
+        'https://upload.wikimedia.org/wikipedia/en/2/2f/Whothehellareyou.jpg',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 9,
+    title: "Groovejet (If This Ain't Love)",
+    artist: 'Spiller feat. Sophie Ellis-Bextor',
+    album: 'Read My Lips',
+    youtubeVideoId: 'FI3gn4APNXc',
+    imageSelection: 'album',
+    imageSources: {
+      album:
+        'https://upload.wikimedia.org/wikipedia/en/1/1b/Spiller-groovejet.jpg',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 10,
+    title: 'Shackles (Praise You)',
+    artist: 'Mary Mary',
+    album: 'Thankful',
+    youtubeVideoId: 'i1L2lToMlB4',
+    imageSelection: 'album',
+    imageSources: {
+      album:
+        'https://upload.wikimedia.org/wikipedia/en/d/dd/Mary_Mary_-_Shackles_%28Praise_You%29%28LQ%29.jpeg',
+      artist: null,
+    },
+  }),
+]
+
+export default songs
---
+++ src/data/years/2001.ts
@@ -0,0 +1,163 @@
+import { getSongThumbnailPath } from '@/data/imageAsset'
+import type { Song } from '@/types/song'
+
+const year = 2001
+
+export const source = {
+  label: 'ARIA Charts',
+  url: 'https://en.wikipedia.org/wiki/List_of_top_25_singles_for_2001_in_Australia',
+}
+
+const getSong = ({
+  rank,
+  title,
+  artist,
+  album,
+  youtubeVideoId,
+  imageSelection,
+  imageSources,
+}: Omit<Song, 'thumbnailPath'>): Song => ({
+  rank,
+  title,
+  artist,
+  album,
+  youtubeVideoId,
+  imageSelection,
+  imageSources,
+  thumbnailPath: getSongThumbnailPath({ year, rank, title }),
+})
+
+// Source: ARIA year-end singles chart for 2001
+const songs: Song[] = [
+  getSong({
+    rank: 1,
+    title: "Can't Fight the Moonlight",
+    artist: 'LeAnn Rimes',
+    album: 'Coyote Ugly',
+    youtubeVideoId: 'bx3s99FNXzI',
+    imageSelection: 'album',
+    imageSources: {
+      album:
+        'https://upload.wikimedia.org/wikipedia/en/thumb/4/4f/Can%27t_Fight_the_Moonlight.jpg/330px-Can%27t_Fight_the_Moonlight.jpg',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 2,
+    title: "It Wasn't Me",
+    artist: 'Shaggy',
+    album: 'Hot Shot',
+    youtubeVideoId: '2g5Hz17C4is',
+    imageSelection: 'album',
+    imageSources: {
+      album:
+        'https://upload.wikimedia.org/wikipedia/en/b/b1/Shaggy-wasn%27t-me.jpg',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 3,
+    title: "Can't Get You Out of My Head",
+    artist: 'Kylie Minogue',
+    album: 'Fever',
+    youtubeVideoId: 'c18441Eh_WE',
+    imageSelection: 'album',
+    imageSources: {
+      album:
+        'https://upload.wikimedia.org/wikipedia/en/6/60/Kylie_Minogue_-_Can%27t_Get_You_Out_of_My_Head.png',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 4,
+    title: 'Angel',
+    artist: 'Shaggy',
+    album: 'Hot Shot',
+    youtubeVideoId: 'XWJrPzAUzAs',
+    imageSelection: 'album',
+    imageSources: {
+      album: 'https://upload.wikimedia.org/wikipedia/en/0/0f/Shaggy-angel.jpg',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 5,
+    title: 'Hanging by a Moment',
+    artist: 'Lifehouse',
+    album: 'No Name Face',
+    youtubeVideoId: 'tPnK39ax_AM',
+    imageSelection: 'album',
+    imageSources: {
+      album:
+        'https://upload.wikimedia.org/wikipedia/en/6/67/Lifehousehangingbyamoment.jpg',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 6,
+    title: 'Stan',
+    artist: 'Eminem feat. Dido',
+    album: 'The Marshall Mathers LP',
+    youtubeVideoId: 'gOMhN-hfMtY',
+    imageSelection: 'album',
+    imageSources: {
+      album:
+        'https://upload.wikimedia.org/wikipedia/en/e/e8/Eminem_-_Stan_CD_cover.jpg',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 7,
+    title: "I'm Like a Bird",
+    artist: 'Nelly Furtado',
+    album: 'Whoa, Nelly!',
+    youtubeVideoId: 'roPQ_M3yJTA',
+    imageSelection: 'album',
+    imageSources: {
+      album:
+        'https://upload.wikimedia.org/wikipedia/en/b/b6/Nelly_Furtado_-_I%27m_Like_a_Bird.jpg',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 8,
+    title: 'Smooth Criminal',
+    artist: 'Alien Ant Farm',
+    album: 'Bad',
+    youtubeVideoId: 'CDl9ZMfj6aE',
+    imageSelection: 'album',
+    imageSources: {
+      album:
+        'https://upload.wikimedia.org/wikipedia/en/3/33/Smooth_Criminal.jpg',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 9,
+    title: 'Can We Fix It?',
+    artist: 'Bob the Builder',
+    album: 'Bob the Builder: The Album',
+    youtubeVideoId: 'l-epqIHe4w0',
+    imageSelection: 'album',
+    imageSources: {
+      album:
+        'https://upload.wikimedia.org/wikipedia/en/0/09/Bob_the_Builder_Can_We_Fix_It_art.jpg',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 10,
+    title: 'Whole Again',
+    artist: 'Atomic Kitten',
+    album: 'Right Now',
+    youtubeVideoId: '1V0xQkk9kbc',
+    imageSelection: 'album',
+    imageSources: {
+      album:
+        'https://upload.wikimedia.org/wikipedia/en/b/b3/Atomic_Kitten_Whole_Again_Cover.jpg',
+      artist: null,
+    },
+  }),
+]
+
+export default songs
