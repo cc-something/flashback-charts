bfd8f9683127d9862477372faa27f434d166539a
Saxon Cameron <github.humble368@passinbox.com>
2026-04-04 12:46:42 +1030

    feat: finish 1993 media

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,34 @@
 import type { Song } from '@/types/song'
+import songs1940, { source as source1940 } from './years/1940'
+import songs1941, { source as source1941 } from './years/1941'
+import songs1948, { source as source1948 } from './years/1948'
+import songs1949, { source as source1949 } from './years/1949'
+import songs1950, { source as source1950 } from './years/1950'
+import songs1951, { source as source1951 } from './years/1951'
+import songs1952, { source as source1952 } from './years/1952'
+import songs1970, { source as source1970 } from './years/1970'
+import songs1971, { source as source1971 } from './years/1971'
+import songs1972, { source as source1972 } from './years/1972'
 import songs1973, { source as source1973 } from './years/1973'
+import songs1974, { source as source1974 } from './years/1974'
+import songs1975, { source as source1975 } from './years/1975'
+import songs1976, { source as source1976 } from './years/1976'
+import songs1977, { source as source1977 } from './years/1977'
+import songs1978, { source as source1978 } from './years/1978'
+import songs1979, { source as source1979 } from './years/1979'
+import songs1990, { source as source1990 } from './years/1990'
+import songs1991, { source as source1991 } from './years/1991'
+import songs1992, { source as source1992 } from './years/1992'
+import songs1993, { source as source1993 } from './years/1993'
+import songs1997, { source as source1997 } from './years/1997'
+import songs2000, { source as source2000 } from './years/2000'
+import songs2001, { source as source2001 } from './years/2001'
+import songs2002, { source as source2002 } from './years/2002'
+import songs2003, { source as source2003 } from './years/2003'
+import songs2010, { source as source2010 } from './years/2010'
+import songs2011, { source as source2011 } from './years/2011'
+import songs2024, { source as source2024 } from './years/2024'
+import songs2025, { source as source2025 } from './years/2025'
 
 export interface YearSource {
   label: string
@@ -12,9 +41,125 @@ interface YearChartData {
 }
 
 const yearData: Record<number, YearChartData> = {
+  1940: {
+    songs: songs1940,
+    source: source1940,
+  },
+  1941: {
+    songs: songs1941,
+    source: source1941,
+  },
+  1948: {
+    songs: songs1948,
+    source: source1948,
+  },
+  1949: {
+    songs: songs1949,
+    source: source1949,
+  },
+  1950: {
+    songs: songs1950,
+    source: source1950,
+  },
+  1951: {
+    songs: songs1951,
+    source: source1951,
+  },
+  1952: {
+    songs: songs1952,
+    source: source1952,
+  },
+  1970: {
+    songs: songs1970,
+    source: source1970,
+  },
+  1971: {
+    songs: songs1971,
+    source: source1971,
+  },
+  1972: {
+    songs: songs1972,
+    source: source1972,
+  },
   1973: {
     songs: songs1973,
     source: source1973,
+  },
+  1974: {
+    songs: songs1974,
+    source: source1974,
+  },
+  1975: {
+    songs: songs1975,
+    source: source1975,
+  },
+  1976: {
+    songs: songs1976,
+    source: source1976,
+  },
+  1977: {
+    songs: songs1977,
+    source: source1977,
+  },
+  1978: {
+    songs: songs1978,
+    source: source1978,
+  },
+  1979: {
+    songs: songs1979,
+    source: source1979,
+  },
+  1990: {
+    songs: songs1990,
+    source: source1990,
+  },
+  1991: {
+    songs: songs1991,
+    source: source1991,
+  },
+  1992: {
+    songs: songs1992,
+    source: source1992,
+  },
+  1993: {
+    songs: songs1993,
+    source: source1993,
+  },
+  1997: {
+    songs: songs1997,
+    source: source1997,
+  },
+  2000: {
+    songs: songs2000,
+    source: source2000,
+  },
+  2001: {
+    songs: songs2001,
+    source: source2001,
+  },
+  2002: {
+    songs: songs2002,
+    source: source2002,
+  },
+  2003: {
+    songs: songs2003,
+    source: source2003,
+  },
+  2010: {
+    songs: songs2010,
+    source: source2010,
+  },
+  2011: {
+    songs: songs2011,
+    source: source2011,
+  },
+  2024: {
+    songs: songs2024,
+    source: source2024,
+  },
+  2025: {
+    songs: songs2025,
+    source: source2025,
   },
 }
 
---
+++ src/data/years/1993.ts
@@ -0,0 +1,163 @@
+import { getSongThumbnailPath } from '@/data/imageAsset'
+import type { Song } from '@/types/song'
+
+const year = 1993
+
+export const source = {
+  label: 'ARIA End of Year singles chart',
+  url: 'https://en.wikipedia.org/wiki/List_of_top_25_singles_for_1993_in_Australia',
+}
+
+const getSong = ({
+  rank,
+  title,
+  artist,
+  album,
+  youtubeVideoId,
+  imageSelection,
+  imageSources,
+}: Omit<Song, 'thumbnailPath'>): Song => ({
+  rank,
+  title,
+  artist,
+  album,
+  youtubeVideoId,
+  imageSelection,
+  imageSources,
+  thumbnailPath: getSongThumbnailPath({ year, rank, title }),
+})
+
+// Source: ARIA End of Year singles chart 1993 year-end chart
+// Via: Wikipedia — List of top 25 singles for 1993 in Australia
+const songs: Song[] = [
+  getSong({
+    rank: 1,
+    title: "I'd Do Anything for Love (But I Won't Do That)",
+    artist: 'Meat Loaf',
+    album: 'Bat Out of Hell II: Back into Hell',
+    youtubeVideoId: '9X_ViIPA-Gc',
+    imageSelection: 'album',
+    imageSources: {
+      album:
+        'https://upload.wikimedia.org/wikipedia/en/7/74/I%27d_Do_Anything_for_Love_%28but_I_Won%27t_Do_That%29_by_Meat_Loaf_US_commercial_cassette.jpg',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 2,
+    title: 'I Will Always Love You',
+    artist: 'Whitney Houston',
+    album: 'The Bodyguard: Original Soundtrack Album',
+    youtubeVideoId: '3JWTaaS7LdU',
+    imageSelection: 'album',
+    imageSources: {
+      album:
+        'https://upload.wikimedia.org/wikipedia/commons/4/48/I_will_always_love_you_by_Dolly_Parton_1974_US_single.png',
+      artist:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Whitney_Houston_Welcome_Home_Heroes_1_cropped.jpg/1280px-Whitney_Houston_Welcome_Home_Heroes_1_cropped.jpg',
+    },
+  }),
+  getSong({
+    rank: 3,
+    title: "You Don't Treat Me No Good",
+    artist: 'Sonia Dada',
+    album: 'Sonia Dada',
+    youtubeVideoId: 'Lqw1Q19i_vQ',
+    imageSelection: 'album',
+    imageSources: {
+      album:
+        'https://upload.wikimedia.org/wikipedia/en/6/65/You_Don%27t_Treat_Me_No_Good_AUSNZ.png',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 4,
+    title: 'Sweat (A La La La La Long)',
+    artist: 'Inner Circle',
+    album: 'Bad to the Bone',
+    youtubeVideoId: '-SvridcLD-I',
+    imageSelection: 'album',
+    imageSources: {
+      album:
+        'https://upload.wikimedia.org/wikipedia/en/e/e6/Sweat_%28a_la_la_la_la_long%29.jpg',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 5,
+    title: "(I Can't Help) Falling in Love with You",
+    artist: 'UB40',
+    album: 'Promises and Lies',
+    youtubeVideoId: 'vUdloUqZa7w',
+    imageSelection: 'album',
+    imageSources: {
+      album:
+        'https://upload.wikimedia.org/wikipedia/en/3/3c/Can%27t_Help_Falling_in_Love_by_Elvis_Presley_US_picture_sleeve.png',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 6,
+    title: 'Informer',
+    artist: 'Snow',
+    album: '12 Inches of Snow',
+    youtubeVideoId: 'TSffz_bl6zo',
+    imageSelection: 'album',
+    imageSources: {
+      album:
+        'https://upload.wikimedia.org/wikipedia/en/2/2f/Snow_Informer_%28Single_Cover%29.jpg',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 7,
+    title: "What's Up",
+    artist: '4 Non Blondes',
+    album: 'Bigger, Better, Faster, More!',
+    youtubeVideoId: '6NXnxTNIWkc',
+    imageSelection: 'album',
+    imageSources: {
+      album: 'https://upload.wikimedia.org/wikipedia/en/e/e1/WhatsUpCover.jpg',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 8,
+    title: "Cat's in the Cradle",
+    artist: 'Ugly Kid Joe',
+    album: "America's Least Wanted",
+    youtubeVideoId: 'B32yjbCSVpU',
+    imageSelection: 'album',
+    imageSources: {
+      album: 'https://upload.wikimedia.org/wikipedia/en/b/b1/Chapin_cradle_cover.jpg',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 9,
+    title: 'All That She Wants',
+    artist: 'Ace of Base',
+    album: 'Happy Nation',
+    youtubeVideoId: 'd73tiBBzvFM',
+    imageSelection: 'album',
+    imageSources: {
+      album:
+        'https://upload.wikimedia.org/wikipedia/en/d/d5/All_That_She_Wants_%28Ace_of_Base_single_-_cover_art%29.jpg',
+      artist: null,
+    },
+  }),
+  getSong({
+    rank: 10,
+    title: 'Please Forgive Me',
+    artist: 'Bryan Adams',
+    album: 'So Far So Good',
+    youtubeVideoId: '9EHAo6rEuas',
+    imageSelection: 'album',
+    imageSources: {
+      album: 'https://upload.wikimedia.org/wikipedia/en/9/90/Please_forgive_me.jpg',
+      artist: null,
+    },
+  }),
+]
+
+export default songs
