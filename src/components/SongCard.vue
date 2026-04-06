60103ff2b7f6fbc04af652d342c57c2e82607ed4
Saxon Cameron <github.humble368@passinbox.com>
2026-04-07 08:00:42 +0930

    fix: allow clicks through song text to play button

--- src/components/SongCard.vue
+++ src/components/SongCard.vue
@@ -126,7 +126,9 @@ const youtubeVideoUrl = computed(() =>
       </Transition>
     </button>
 
-    <div class="theme-body relative z-10 min-w-0 flex-1 flex flex-col gap-0.5">
+    <div
+      class="theme-body pointer-events-none relative z-10 min-w-0 flex-1 flex flex-col gap-0.5"
+    >
       <h2 class="text-base font-bold leading-snug text-text">
         {{ song.title }}
       </h2>
