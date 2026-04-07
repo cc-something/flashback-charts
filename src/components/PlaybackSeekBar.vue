022e43c467adcbb670599beb14c7f2cff9af72e1
Saxon Cameron <github.humble368@passinbox.com>
2026-04-07 14:43:44 +0930

    fix: slightly larger seek thumb

--- src/components/PlaybackSeekBar.vue
+++ src/components/PlaybackSeekBar.vue
@@ -41,7 +41,7 @@ const progressPercent = computed(() => {
       />
     </SliderTrack>
     <SliderThumb
-      class="playback-seek-thumb block h-2 w-2 rounded-full border border-white/90 bg-primary shadow-[0_1px_3px_rgb(0_0_0_/_0.25)] outline-none"
+      class="playback-seek-thumb block h-2.5 w-2.5 rounded-full border border-white/90 bg-primary shadow-[0_1px_3px_rgb(0_0_0_/_0.25)] outline-none"
     />
   </SliderRoot>
 </template>
