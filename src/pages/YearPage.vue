498f9f25266ba489d390c33b86cda12c6a921c1b
Saxon Cameron <github.humble368@passinbox.com>
2026-04-07 14:58:41 +0930

    style: round first/last song rows and tighten gap on year pages

--- src/pages/YearPage.vue
+++ src/pages/YearPage.vue
@@ -203,7 +203,7 @@ watch(
       @after-leave="applyPendingTheme"
     >
       <div :key="yearNumber" class="year-content">
-        <div v-if="store.hasData" class="flex flex-col gap-1.5">
+        <div v-if="store.hasData" class="flex flex-col gap-px">
           <SongCard
             v-for="song in store.currentSongs"
             :key="`${yearNumber}-${song.rank}`"
