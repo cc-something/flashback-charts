e2f80981c7f3c726f5b138e3efd4c6bd7b07af75
Saxon Cameron <github.humble368@passinbox.com>
2026-04-07 22:19:31 +0930

    perf: split route bundles

--- src/router.ts
+++ src/router.ts
@@ -1,17 +1,22 @@
 import type { RouteRecordRaw } from 'vue-router'
-import HomePage from '@/pages/HomePage.vue'
-import DecadePage from '@/pages/DecadePage.vue'
-import YearPage from '@/pages/YearPage.vue'
-import NotFound from '@/pages/NotFound.vue'
 
 export const routes: RouteRecordRaw[] = [
-  { path: '/', name: 'home', component: HomePage },
+  { path: '/', name: 'home', component: () => import('@/pages/HomePage.vue') },
   {
     path: '/:decade(\\d{4}s)',
     name: 'decade',
-    component: DecadePage,
+    component: () => import('@/pages/DecadePage.vue'),
     props: true,
   },
-  { path: '/:year(\\d{4})', name: 'year', component: YearPage, props: true },
-  { path: '/:pathMatch(.*)*', name: 'not-found', component: NotFound },
+  {
+    path: '/:year(\\d{4})',
+    name: 'year',
+    component: () => import('@/pages/YearPage.vue'),
+    props: true,
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'not-found',
+    component: () => import('@/pages/NotFound.vue'),
+  },
 ]
