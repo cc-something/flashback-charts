e0c805aedb4e76cc3418371adbc5cae95fbeac81
Saxon Cameron <github.humble368@passinbox.com>
2026-04-09 17:05:48 +1000

    style: reposition song card actions as hover overlay

--- src/components/SongCard.vue
+++ src/components/SongCard.vue
@@ -175,11 +175,11 @@ const youtubeVideoUrl = computed(() =>
     </div>
 
     <div
-      class="relative z-20 flex self-center flex-shrink-0 flex-col -space-y-0.5"
+      class="absolute top-1.5 right-1.5 z-20 flex flex-row gap-0.5 opacity-0 transition-opacity duration-150 group-hover:opacity-100"
     >
       <div v-if="displaySong.youtubeVideoId" class="group/action relative">
         <span
-          class="pointer-events-none absolute right-full top-1/2 mr-1 -translate-y-1/2 whitespace-nowrap rounded-md bg-black/80 px-2 py-1 text-[0.7rem] font-medium text-white opacity-0 transition-opacity duration-75 group-hover/action:opacity-100 group-focus-within/action:opacity-100"
+          class="pointer-events-none absolute right-full top-1/2 mr-1 -translate-y-1/2 whitespace-nowrap rounded bg-black/80 px-1.5 py-0.5 text-[0.65rem] font-medium text-white opacity-0 transition-opacity duration-75 group-hover/action:opacity-100 group-focus-within/action:opacity-100"
         >
           YouTube
         </span>
@@ -196,9 +196,9 @@ const youtubeVideoUrl = computed(() =>
 
       <div class="group/action relative">
         <span
-          class="pointer-events-none absolute right-full top-1/2 mr-1 -translate-y-1/2 whitespace-nowrap rounded-md bg-black/80 px-2 py-1 text-[0.7rem] font-medium text-white opacity-0 transition-opacity duration-75 group-hover/action:opacity-100 group-focus-within/action:opacity-100"
+          class="pointer-events-none absolute right-full top-1/2 mr-1 -translate-y-1/2 whitespace-nowrap rounded bg-black/80 px-1.5 py-0.5 text-[0.65rem] font-medium text-white opacity-0 transition-opacity duration-75 group-hover/action:opacity-100 group-focus-within/action:opacity-100"
         >
-          Report an issue with this song
+          Report issue
         </span>
         <button
           type="button"
